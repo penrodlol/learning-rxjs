2519d2366e360c374c642504f223e523c948af1b
penrodlol <penrod.christian7984@gmail.com>
2020-01-24 22:16:59 -0500

    Adding initial splash screen and core color scheme

--- learning-rxjs/src/app/app.module.ts
+++ learning-rxjs/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { SubjectsModule } from './subjects/subjects.module';
     ConceptsModule,
     OperatorsModule,
     RecipesModule,
-    SubjectsModule
+    SubjectsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
